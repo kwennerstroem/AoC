c40bb36f1431c48bcbcfac8d8899e2e6b6f25072
Kevin Wennerström <118304190+kwennerstroem@users.noreply.github.com>
2025-12-01 11:34:51 +0000

    2025: Day01

---
+++ 2025/Day01/Program.cs
@@ -0,0 +1,70 @@
+﻿int PartOne(string[] input)
+{
+    var currentPosition = 50;
+    var positionZeroCount = 0;
+
+    foreach (var item in input)
+    {
+        var isRightToLeft = item[..1] == "L";
+        if (isRightToLeft)
+        {
+            currentPosition -= int.Parse(item[1..]);
+        }
+        else
+        {
+            currentPosition += int.Parse(item[1..]);
+        }
+
+        currentPosition = ((currentPosition % 100) + 100) % 100;
+
+        if (currentPosition == 0)
+        {
+            positionZeroCount++;
+        }
+    }
+
+    return positionZeroCount;
+}
+
+int PartTwo(string[] input)
+{
+    var currentPosition = 50;
+    var positionZeroCount = 0;
+
+    foreach (var item in input)
+    {
+        var isRightToLeft = item[..1] == "L";
+        var fullSpins = int.Parse(item[1..]) / 100;
+        var restSpin = int.Parse(item[1..]) % 100;
+
+        positionZeroCount += fullSpins;
+
+        if (isRightToLeft)
+        {
+            if (currentPosition != 0 && currentPosition - restSpin <= 0)
+            {
+                positionZeroCount++;
+            }
+
+            currentPosition -= int.Parse(item[1..]);
+        }
+        else
+        {
+            if (currentPosition != 0 && currentPosition + restSpin >= 100)
+            {
+                positionZeroCount++;
+            }
+
+            currentPosition += int.Parse(item[1..]);
+        }
+
+        currentPosition = ((currentPosition % 100) + 100) % 100;
+    }
+
+    return positionZeroCount;
+}
+
+var input = File.ReadAllLines("input.txt");
+
+Console.WriteLine(PartOne(input));
+Console.WriteLine(PartTwo(input));
